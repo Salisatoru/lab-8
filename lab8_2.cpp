a04b2d87e6b6905acbcb3497cad2d49d6c377743
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2021-12-03 18:36:11 +0700

    lab8_2.cpp.completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+    int sc;
+    string name, movie, day, motto;
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout<<"?????: ";
+    getline(cin,name);
+    
+    cout<<"Fahsai: Wow!!! "<< name <<" is a really cool name.\n";
+    cout<<"Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout<<name<<": ";
+    cin>>sc;
+    cin.ignore();
+    
+    cout<<"Fahsai: I think you may be GEAR "<<(sc/10000000)-12<<". I have a free movie ticket for you.\n";
+    cout<<"Fahsai: Let's go to the cinema together!!!\n";
+    cout<<"Fahsai: What movie do you want to watch?\n";
+    cout<<name<<": ";
+    getline(cin,movie);
+    
+    cout<<"Fahsai: So....which day are you free to go with me?\n";
+    cout<<name<<": ";
+    getline(cin,day);
+    cout<<"Fahsai: " <<day<< "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+    cout <<name<<": ";
+    getline(cin,motto);
+    cout<<"Fahsai: 555+ see you "<<day<<". Bye Bye \\(^ ^)/";
+}
 
