e916d564a10fc9c46ab57fe66689dd8d3f46ac39
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2021-12-03 18:56:25 +0700

    lab8_3.cpp.completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+
+char before(char x){
+    if(x=='A'){
+        return 'Z';
+    }
+    if('A'<x and x<='Z'){
+        return x-1;
+    }else{
+        return '0';
+    }
+}
+
 
