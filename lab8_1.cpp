eeae6da37e4e7f35471e66ebf12afa6476ee58e2
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2021-12-03 15:53:52 +0700

    lab8_1.cpp.completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    double a  =  20; double b = 30;
+    double c = 4, output;
+
+    output = a - b/c;
+    cout << "output: " << output << endl << "\n";
+    return 0;
+}
 
